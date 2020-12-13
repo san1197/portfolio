64a1eff3fa6111df22975363d4cbd5daa5b6c6b6
Sandya Manoharan <smanoha9@sundevils.asu.edu>
2020-12-12 19:08:24 -0800

    Updates

---
+++ precache-manifest.6e07a237eab3b960c7113fc12be12aee.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a35d87ab49889e680aef0b8fa1972e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d05b0994bb56cb1d199",
+    "url": "/portfolio/static/css/3.ecdb89c8.chunk.css"
+  },
+  {
+    "revision": "6b048399ed7fdf528396",
+    "url": "/portfolio/static/css/main.aca18c63.chunk.css"
+  },
+  {
+    "revision": "4b0bbcad2fec9b4f925a",
+    "url": "/portfolio/static/js/2.8ed1045f.chunk.js"
+  },
+  {
+    "revision": "3d05b0994bb56cb1d199",
+    "url": "/portfolio/static/js/3.bbe4a6b6.chunk.js"
+  },
+  {
+    "revision": "6b048399ed7fdf528396",
+    "url": "/portfolio/static/js/main.68e610f5.chunk.js"
+  },
+  {
+    "revision": "4170223ee55165f77565",
+    "url": "/portfolio/static/js/runtime-main.65f14f28.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "036c2c488120e4814a46359fa721b6b0",
+    "url": "/portfolio/static/media/AndBasR.036c2c48.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3a8c0bed0e1fa47e92ff3e655bfa0bd4",
+    "url": "/portfolio/static/media/aesLogo.3a8c0bed.png"
+  },
+  {
+    "revision": "bf95aee7c84f8488e633ca3cd78e32c8",
+    "url": "/portfolio/static/media/aicopLogo.bf95aee7.png"
+  },
+  {
+    "revision": "baa352d6e1c2c79a97e7284fc2f65ab4",
+    "url": "/portfolio/static/media/asuLogo.baa352d6.png"
+  },
+  {
+    "revision": "778b8185e1bfac5619220ac3759b41ca",
+    "url": "/portfolio/static/media/awsLogo.778b8185.png"
+  },
+  {
+    "revision": "074139b4be8a3ffbbb11ae5be826a99c",
+    "url": "/portfolio/static/media/codezillaLogo.074139b4.png"
+  },
+  {
+    "revision": "ee2baeb4a7544f6982b695266d4fcc2d",
+    "url": "/portfolio/static/media/contactImage.ee2baeb4.png"
+  },
+  {
+    "revision": "97539cea3589c1926db96dc74e3233ff",
+    "url": "/portfolio/static/media/developerImage.97539cea.png"
+  },
+  {
+    "revision": "8007472b9bf60c0ac2718668a5ed6347",
+    "url": "/portfolio/static/media/expenseLogo.8007472b.png"
+  },
+  {
+    "revision": "9c2452ee4581f1e948af6761f8e9c196",
+    "url": "/portfolio/static/media/mathubLogo.9c2452ee.png"
+  },
+  {
+    "revision": "64b22d6b7364180c6f773c1eff0521f4",
+    "url": "/portfolio/static/media/opphackLogo.64b22d6b.png"
+  },
+  {
+    "revision": "4eb192285e63cee033ecf488ed254d86",
+    "url": "/portfolio/static/media/opphackLogo2.4eb19228.png"
+  },
+  {
+    "revision": "d634552f19bfc13c490ddc87825c9526",
+    "url": "/portfolio/static/media/proficiencyImage.d634552f.png"
+  },
+  {
+    "revision": "97d4e083a7881c571e0c5b0e13d10f1a",
+    "url": "/portfolio/static/media/skillsImage.97d4e083.png"
+  }
+]);
